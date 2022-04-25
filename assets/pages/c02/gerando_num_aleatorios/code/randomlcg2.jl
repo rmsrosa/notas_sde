ad3a636817b0598095766dcf7ebfc6cf9da0cfbc
rmsrosa <rmsrosa@gmail.com>
2022-04-25 12:21:46 +0000

    Deploying to gh-pages from @ rmsrosa/notas_sde@66edd44f157449b8f9e108c9c556f2d007f184d3 🚀

---
+++ assets/pages/c02/gerando_num_aleatorios/code/randomlcg2.jl
@@ -0,0 +1,21 @@
+# This file was generated, do not modify it. # hide
+#hideall
+using Plots
+theme(:ggplot2)
+a = 7
+b = 0
+m = 2^13 - 1
+x0 = 1
+
+X = Vector{Float64}(undef, m - 1)
+X[1] = x0 
+for i in 2:m-1
+    X[i] = mod( a * X[i-1] + b, m)
+end
+
+p1 = scatter(1:m-1, X, title = "Sequência gerada\n(a = $a, b = $b, m = $m)", titlefont = 10, label = false, markersize = 1)
+
+p2 = histogram(X, label = false, xaxis = "X", yaxis = "contagem", title = "Histograma", titlefont = 10, bins = 50)
+
+plot(p1, p2, layout = 2, size = (800, 400))
+savefig(joinpath(@OUTPUT, "randomlcg2.svg"))
