377221748f7b206cfd2323f555e1ce9205c3a8b8
rmsrosa <rmsrosa@gmail.com>
2022-05-09 16:40:51 +0000

    Deploying to gh-pages from @ rmsrosa/notas_sde@955d5d772314186d68d461aa30b5c52658874d92 🚀

---
+++ assets/pages/c03/tipos_processos/code/diffwiener_hatfunction.jl
@@ -0,0 +1,13 @@
+# This file was generated, do not modify it. # hide
+#hideall
+using Plots
+theme(:ggplot2)
+t0 = 0.0
+t1 = 4.0
+s = 2.0
+h = 0.5
+tt = range(t0, t1, length = 200)
+dh(t, s, h) = (min(t+h, s+h) - min(t, s+h) - min(t+h, s) + min(t,s))/h^2
+
+plot(tt, t -> dh(t, s, h), xaxis = "tempo", yaxis = "valor", title = "Correlação dₕ(t, s) das diferenças finitas de um processo de Wiener, com s = $s", titlefont = 8, label = false)
+savefig(joinpath(@OUTPUT, "diffwiener_hatfunction.svg"))
