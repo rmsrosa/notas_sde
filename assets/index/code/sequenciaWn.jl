abadd21c5552367f6946d943703f622193f12530
rmsrosa <rmsrosa@gmail.com>
2022-05-18 22:41:27 +0000

    Deploying to gh-pages from @ rmsrosa/notas_sde@03b54d8d4e3a0ab6ac831662d62046379b5dc66b 🚀

---
+++ assets/index/code/sequenciaWn.jl
@@ -0,0 +1,18 @@
+# This file was generated, do not modify it. # hide
+#hideall
+using Plots
+using Random
+theme(:ggplot2)
+rng = Xoshiro(123)
+N = 3
+plts = []
+J = 8
+ttn = [[j/2^(n-1) for j in 0:2^(n-1)] for n in 1:J]
+Wn = [[0, randn(rng)]]
+for n in 2:J
+    push!(Wn, zeros(2^(n-1) + 1))
+    Wn[end][begin:2:end] .= Wn[end-1]
+    Wn[end][begin+1:2:end-1] .= (Wn[end][begin:2:end-2] + Wn[end][begin+2:2:end])/2 .+ randn(rng, 2^(n-2))
+end
+plot(ttn, Wn, title = "Caminhos amostrais da sequência aproximante \$\\{W_d^n\\}_{d\\in D_n}\$, \$n = 1, \\ldots, $J\$, de um processo de Wiener", titlefont = 10, xaxis = "\$t\$", yaxis = "\$W\$", ylims = (-5, 5), marker = :circle, markersize = 3, label = false, size = (800, 500))
+savefig(joinpath(@OUTPUT, "sequenciaWn.svg"))
