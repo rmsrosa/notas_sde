40e09317fbdfb965c2585ce3996901a9b417f836
rmsrosa <rmsrosa@gmail.com>
2023-04-17 18:33:36 +0000

    Deploying to gh-pages from @ rmsrosa/notas_sde@2a31c1b254df6faad95479db1f8826cb4af59d2e 🚀

--- assets/pages/c05/existencia_processo_wiener/code/faberschauder.jl
+++ assets/pages/c05/existencia_processo_wiener/code/faberschauder.jl
@@ -3,21 +3,11 @@
 using Plots
 theme(:ggplot2)
 t = range(0.0, 1.0, length = 200)
-s_1 = copy(t) ## n = 1
-s_1_2 = 2^(1/2) * min.(t, 1 .- t) ## n  = 2
-s_1_4 = 2 * max.(0, min.(t, 1/2 .- t)) ## n = 3
-s_3_4 = 2 * max.(0, min.(t .- 1/2, 1 .- t)) ## n = 3
-s_1_8 = 2^(3/2) * max.(0, min.(t, 1/4 .- t)) ## n = 4
-s_3_8 = 2^(3/2) * max.(0, min.(t .- 1/4, 1/2 .- t)) ## n = 4
-s_5_8 = 2^(3/2) * max.(0, min.(t .- 1/2, 3/4 .- t)) ## n = 4
-s_7_8 = 2^(3/2) * max.(0, min.(t .- 3/4, 1 .- t)) ## n = 4
-plot(title = "Funções da base de Faber–Schauder", titlefont = 10, xaxis = "\$t\$", ylims = (0.0, 1.0), legend = :topleft)
-plot!(t, s_1, label = "\$s_1(t)\$")
-plot!(t, s_1_2, label = "\$s_{1/2}(t)\$")
-plot!(t, s_1_4, label = "\$s_{1/4}(t)\$")
-plot!(t, s_3_4, label = "\$s_{3/4}(t)\$")
-plot!(t, s_1_8, label = "\$s_{1/8}(t)\$")
-plot!(t, s_3_8, label = "\$s_{3/8}(t)\$")
-plot!(t, s_5_8, label = "\$s_{5/8}(t)\$")
-plot!(t, s_7_8, label = "\$s_{7/8}(t)\$")
+s_d = Dict(n => Dict(d => 2^(max(0,(n-1)/2)) .* max.(0.0, min.(t .- d .+ 1/2^n, d .+ 1/2^n .- t)) for d in Rational.(0:1/2^(n):1) if (n == 0 || d ∉ Rational.(0:1/2^(n-1):1))) for n in 0:4)
+plot(title = "Funções da base de Faber–Schauder", titlefont = 10, xaxis = "\$t\$", ylims = (0.0, 1.0), legend = :topleft, legendfontsize=6)
+for (n, sdn) in sort(s_d)
+    for (d, sd) in sort(sdn)
+        plot!(t, sd, label="\$s_{$d}(t)\$")
+    end
+end
 savefig(joinpath(@OUTPUT, "faberschauder.svg"))
