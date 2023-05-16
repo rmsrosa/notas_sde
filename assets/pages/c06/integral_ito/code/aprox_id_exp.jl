c75d112621a96403ce098b26f7db349ed6e96dfe
rmsrosa <rmsrosa@gmail.com>
2023-05-16 19:40:53 +0000

    Deploying to gh-pages from @ rmsrosa/notas_sde@f0d133516ca7067aae0016fd3ebd06b67e7008a0 🚀

--- assets/pages/c06/integral_ito/code/aprox_id_exp.jl
+++ assets/pages/c06/integral_ito/code/aprox_id_exp.jl
@@ -5,7 +5,7 @@ theme(:ggplot2)
 N = 3
 tt = -4:0.01:4
 xx = [
-    m * ifelse.(tt .≤ 0, exp.( m * tt), 0) for m in 1:N
+    m * ifelse.(tt .≥  0, exp.( -m * tt), 0) for m in 1:N
 ]
 plot(tt, xx, title = "aproximações da identidade \$t \\rightarrow m e^{m t}\$", titlefont = 10, xaxis = "\$t\$", yaxis = "\$x\$", label=reshape("m = " .* string.(1:N), 1, :))
 savefig(joinpath(@OUTPUT, "aprox_id_exp.svg"))
