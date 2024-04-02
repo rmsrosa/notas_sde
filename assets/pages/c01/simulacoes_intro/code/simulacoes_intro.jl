d235537ded130e9ae13a1d18ffbc86f00dbf61c2
rmsrosa <rmsrosa@gmail.com>
2024-04-02 20:34:58 +0000

    Deploying to gh-pages from @ rmsrosa/notas_sde@5bca400597fae893a086ebbd23d7006b03e8cb38 🚀

--- assets/pages/c01/simulacoes_intro/code/simulacoes_intro.jl
+++ assets/pages/c01/simulacoes_intro/code/simulacoes_intro.jl
@@ -5,12 +5,12 @@
 for m in 1:M
     x[1, m] = x₀
     for n in 2:N+1
-        dw = randn(rng) * Δt^0.5
+        dw = randn(rng) * √Δt
         x[n, m] = (1.0 + μ̄ * Δt) * x[n-1, m] + σ * x[n-1, m] * dw
     end
 end
 
-plot(t, x, alpha = 0.2, title="Soluções SDE (x₀ = $x₀, μ̄ = $μ̄, σ = $σ, dW_t = N(0, Δt), T = $T, Δt = $Δt)", titlefont = 9, xlabel = "t", ylabel="x", label=permutedims(["soluções"; fill(nothing, M-1)]), color=1)
+plot(t, x, alpha = 0.2, title="Soluções SDE \$(x_0 = $x₀, \\bar\\mu = $μ̄, \\sigma = $σ, dW_t \\sim \\mathcal{N}(0, \\Delta t), T = $T, Δt = $Δt)\$", titlefont = 9, xlabel = "t", ylabel="x", label=permutedims(["soluções"; fill(nothing, M-1)]), color=1)
 plot!(t, x[:, 1], label="uma realização", color=2)
 
 savefig(joinpath(@OUTPUT, "intro_uma_realizacao_sde.svg")) # hide
