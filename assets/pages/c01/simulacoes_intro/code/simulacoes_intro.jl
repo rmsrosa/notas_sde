8b7ac1c32f27dadf57a7a3e6751e0115d4b1376a
rmsrosa <rmsrosa@gmail.com>
2024-04-03 16:33:40 +0000

    Deploying to gh-pages from @ rmsrosa/notas_sde@f161e1ec8a0ae3f96d31b17e5f8974e357e55c05 🚀

--- assets/pages/c01/simulacoes_intro/code/simulacoes_intro.jl
+++ assets/pages/c01/simulacoes_intro/code/simulacoes_intro.jl
@@ -11,6 +11,7 @@ for m in 1:M
 end
 
 plot(t, x, alpha = 0.2, title="Soluções SDE \$(x_0 = $x₀, \\bar\\mu = $μ̄, \\sigma = $σ, dW_t \\sim \\mathcal{N}(0, \\Delta t), T = $T, Δt = $Δt)\$", titlefont = 9, xlabel = "t", ylabel="x", label=permutedims(["soluções"; fill(nothing, M-1)]), color=1)
-plot!(t, x[:, 1], label="uma realização", color=2)
+plot!(t, x[:, 1], label="uma realização", linewidth=2, color=2)
+plot!(t, sum(x, dims=2)/size(x, 2), linewidth=3, color=:orange, label="média")
 
 savefig(joinpath(@OUTPUT, "intro_uma_realizacao_sde.svg")) # hide
