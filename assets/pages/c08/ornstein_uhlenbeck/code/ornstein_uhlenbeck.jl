22866c62e03a2147b8009b82bdda22a544e22f09
rmsrosa <rmsrosa@gmail.com>
2024-11-04 15:55:05 +0000

    Deploying to gh-pages from @ rmsrosa/notas_sde@1bf3ef0e12fa333297d2529558d897b824246236 🚀

--- assets/pages/c08/ornstein_uhlenbeck/code/ornstein_uhlenbeck.jl
+++ assets/pages/c08/ornstein_uhlenbeck/code/ornstein_uhlenbeck.jl
@@ -1,23 +1,52 @@
 # This file was generated, do not modify it. # hide
 #hideall
+using Plots
+using Random
+theme(:ggplot2)
+rng = Xoshiro(123)
 
-N = 600
+M = 100
+N = 200
+t0 = 0.0
+tf = 10.0
 tt = range(t0, tf, length = N)
 dt = Float64(tt.step)
-νs = (0.1, 1.0, 10.0, 100.0)
-Yt = Matrix{Float64}(undef, N, length(νs))
-Yt[1, :] .= 1.0
-for (j, ν) in enumerate(νs)
-    σj = sqrt(2ν)
-    for n in 2:N
-        local dW = √dt * randn(rng)
-        Yt[n, j] = (
-            Yt[n-1, j]
-            - ν * Yt[n-1, j] * dt
-            + σj * dW
-        )
-    end
+
+ν, σ = 1.5, 0.2
+x0 = 0.1
+y0 = 1.0
+
+Wt = Matrix{Float64}(undef, N, M)
+Wt[1, :] .= 0
+for n in 2:N
+    Wt[n, :] .= Wt[n-1, :] .+ √dt * randn(rng, M)
 end
 
-plot(tt, Yt, xaxis = "\$t\$", yaxis = "\$O_t\$", title = "Ornstein-Uhlenbeck aproximando ruído branco", titlefont = 10, label = permutedims(["ν = $ν" for ν in νs]), legend = :topright, alpha=permutedims([1.0 - j/7 for j in 1:length(νs)]))
-savefig(joinpath(@OUTPUT, "ornstein_uhlenbeck_approx_white_noise.svg"))
+Xt = Matrix{Float64}(undef, N, M)
+Yt = Matrix{Float64}(undef, N, M)
+Xt[1, :] .= x0
+Yt[1, :] .= y0
+
+y = y0 .* exp.(-ν .* tt)
+x = x0 .+ y0 .* (1 .- exp.(-ν .* tt)) ./ ν
+
+dWt = zeros(M)
+for n in 2:N
+    dWt .= √dt * randn(rng, M)
+    Yt[n, :] .= (
+        Yt[n-1, :]
+        .- ν * Yt[n-1, :] * dt
+        .+ σ .* dWt
+    )
+    Xt[n, :] .= Xt[n-1, :] .+ Yt[n-1, :] * dt
+end
+
+plot(tt, Yt, xaxis = "\$t\$", yaxis = "\$Y_t\$", title = "Velocidade \$\\{Y_t\\}_t\$ de acordo com\nOrnstein-Uhlenbeck \$\\mathrm{d}Y_t = -\\nu Y_t \\;\\mathrm{d}t + \\sigma Y_t \\;\\mathrm{d}W_t\$,\ncom ν = $ν, σ = $σ, \$Y_0 = \$ $y0", titlefont = 10, label = permutedims(["caminhos amostrais"; fill(nothing, M-1)]), color = 1, alpha = 0.1, legend = :topright)
+plot!(tt, Yt[:, 1], color = 2, label = "um caminho amostral")
+plot!(tt, y, color = 3, label = "valor esperado")
+savefig(joinpath(@OUTPUT, "ornstein_uhlenbeck_vel.svg"))
+
+plot(tt, Xt, xaxis = "\$t\$", yaxis = "\$X_t\$", title = "Posição \$\\{X_t\\}_t\$ de acordo com o sistema\n\$\\mathrm{d}X_t = Y_t\\;\\mathrm{d}t\$ e \$\\mathrm{d}Y_t = -\\nu Y_t \\;\\mathrm{d}t + \\sigma Y_t \\;\\mathrm{d}W_t\$,\ncom ν = $ν, σ = $σ, \$X_0 = \$ $x0, \$Y_0 = \$ $y0", titlefont = 10, label = permutedims(["caminhos amostrais"; fill(nothing, M-1)]), color = 1, alpha = 0.1, legend = :topleft)
+plot!(tt, Xt[:, 1], color = 2, label = "um caminho amostral")
+plot!(tt, x, color = 3, label = "valor esperado")
+savefig(joinpath(@OUTPUT, "ornstein_uhlenbeck_pos.svg"))
--- assets/pages/c08/ou_aproxima_ruidobranco/code/ou_approx_whitenoise.jl
+++ assets/pages/c08/ou_aproxima_ruidobranco/code/ou_approx_whitenoise.jl
@@ -1,4 +1,5 @@
 # This file was generated, do not modify it. # hide
+#hideall
 plts = [plot(abs2.(rfft(ou_t)./n), xlabel="\$k\$", ylabel="\$\\hat y\$", guidefont=10, label="OU τ = $τ spectrum", legend=:topright) for (τ, ou_t) in zip(τs, ous_t)]
 plot(plts..., layout=(div(length(plts), 2), 2), size=(900, 600))
 savefig(joinpath(@OUTPUT, "ou_approx_whitenoise_spectrum.svg"))
